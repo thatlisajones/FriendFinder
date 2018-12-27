2319877d8529a5f1d4eaf7e2b05cdb9455a2b300
That Lisa <thatlisajones@gmail.com>
2018-12-27 14:20:25 -0700

    Adding data

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    return res.json("../data/friends.js");
+  });
+
+  app.post("/api/friends", function(req, res) {
+      waitlist.push(req.body);
+    //   compare friends function here res.json here
+  });
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// apiRoutes(app);
+htmlRoutes(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
