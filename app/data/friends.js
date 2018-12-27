77ec63b40d3c8c6258752d55ee6871b299e5fc34
That Lisa <thatlisajones@gmail.com>
2018-12-27 14:44:06 -0700

    Adding data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,194 +1,194 @@
 var friends=[
   {
-    "name":"Sward",
+    "name":"Anvil Sward",
     "photo":"/images/ai_face_1.jpg",
     "scores":[
         5,
-        1,
-        4,
-        4,
-        5,
-        1,
         2,
         5,
+        3,
+        1,
+        2,
+        3,
         4,
+        1,
         1
       ]
   },
   {
-    "name":"Jelstead",
+    "name":"Jo Jelstead",
     "photo":"/images/ai_face_2.jpg",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
         1,
         2,
+        3,
         5,
         4,
+        3,
+        2,
+        2,
+        1,
+        5
+      ]
+  },
+  {
+    "name":"ZeZe Barzin",
+    "photo":"/images/ai_face_3.jpg",
+    "scores":[
+        3,
+        2,
+        1,
+        1,
+        2,
+        3,
+        4,
+        4,
+        5,
+        5
+      ]
+  },
+  {
+    "name":"Welton Wile",
+    "photo":"/images/ai_face_4.jpg",
+    "scores":[
+        2,
+        4,
+        5,
+        5,
+        3,
+        4,
+        1,
+        3,
+        3,
+        2
+      ]
+  },
+  {
+    "name":"Jarma Kitzch",
+    "photo":"/images/ai_face_5.jpg",
+    "scores":[
+        4,
+        3,
+        5,
+        2,
+        1,
+        3,
+        5,
+        2,
+        1,
+        3
+      ]
+  },
+  {
+    "name":"Barrier Phillips",
+    "photo":"/image/ai_face_6.jpg",
+    "scores":[
+        3,
+        2,
+        3,
+        2,
+        5,
+        5,
+        5,
+        1,
+        1,
+        4
+      ]
+  },
+  {
+    "name":"Westerley Cragg-Smith",
+    "photo":"/images/ai_face_7.jpg",
+    "scores":[
+        3,
+        2,
+        3,
+        5,
+        4,
+        5,
+        4,
+        3,
+        2,
         1
       ]
   },
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Bille Lavendor",
+    "photo":"/images/ai_face_8.jpg",
     "scores":[
         5,
-        1,
         4,
-        4,
-        5,
-        1,
+        3,
         2,
+        1,
         5,
         4,
+        3,
+        2,
         1
       ]
   },
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Rockwell D. Jones",
+    "photo":"/images/ai_face_9.jpg",
     "scores":[
-        5,
         1,
-        4,
-        4,
-        5,
+        2,
+        3,
         1,
         2,
         5,
         4,
-        1
+        5,
+        4,
+        3
       ]
   },
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Scottie Von Melrose",
+    "photo":"/images/ai_face_10.jpg",
     "scores":[
         5,
-        1,
         4,
-        4,
-        5,
-        1,
+        3,
+        3,
         2,
-        5,
-        4,
-        1
+        2,
+        1,
+        1,
+        3,
+        3
       ]
   },
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Indigo Marks",
+    "photo":"/image/ai_face_11.jpg",
     "scores":[
-        5,
-        1,
-        4,
+        2,
         4,
         5,
         1,
         2,
-        5,
         4,
-        1
+        3,
+        3,
+        4,
+        2
       ]
   },
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Quellen Darner",
+    "photo":"/images/ai_face_12.jpg",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
         2,
-        5,
         4,
-        1
-      ]
-  },
-  {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
+        3,
         1,
         4,
-        4,
+        5,
         5,
         1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
-  {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
-  {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
-  {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
-  {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        3,
+        4
       ]
   }
   ]
