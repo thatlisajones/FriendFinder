7f53bd6e93ece86fc25235beae5bc3d640611be6
That Lisa <thatlisajones@gmail.com>
2019-01-04 14:10:49 -0700

    Modal sort of works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,14 @@
 var path = require("path");
+var friends = require("../data/friends")
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
-    return res.json("../data/friends.js");
+    res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
-      friends.push(req.body);
+    console.log("req.body in api routes",req.body);
+    
+      // friends.push(req.body);
     //   compare friends function here res.json here
   });}
