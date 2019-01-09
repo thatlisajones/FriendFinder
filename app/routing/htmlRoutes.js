a29bc6d976d012a7b6fe9543b7ffc18ba98f5082
That Lisa <thatlisajones@gmail.com>
2019-01-09 15:20:50 -0700

    Resolving html routing issue

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -17,7 +17,7 @@
 var path = require("path");
 
 module.exports = function(app) {
-  app.get("/survey", function(req, res) {
+  app.get("/survey.html", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
