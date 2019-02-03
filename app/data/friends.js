7a385e9eed47cdce06ef8223a8fa770172535bab
That Lisa <thatlisajones@gmail.com>
2019-02-03 12:58:54 -0700

    Change survey to dropdown

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
   {
     "name":"Hockey Star Anvil Sward",
-    "photo":"../data/images/ai_face_1.jpg",
+    "photo":"./images/ai_face_1.jpg",
     "questions":[
         5,
         2,
@@ -17,7 +17,7 @@ var friends = [
   },
   {
     "name":"Pet Psychologist Jo Jelstead",
-    "photo":"../data/images/ai_face_2.jpg",
+    "photo":"./images/ai_face_2.jpg",
     "questions":[
         1,
         2,
@@ -33,7 +33,7 @@ var friends = [
   },
   {
     "name":"Housewares Model ZeZe Barzin",
-    "photo":"../data/images/ai_face_3.jpg",
+    "photo":"./images/ai_face_3.jpg",
     "questions":[
         3,
         2,
@@ -49,7 +49,7 @@ var friends = [
   },
   {
     "name":"Author Welton Wile",
-    "photo":"../data/images/ai_face_4.jpg",
+    "photo":"./images/ai_face_4.jpg",
     "questions":[
         2,
         4,
@@ -65,7 +65,7 @@ var friends = [
   },
   {
     "name":"Disgraced Senator Jarma Kitzch",
-    "photo":"../data/images/ai_face_5.jpg",
+    "photo":"./images/ai_face_5.jpg",
     "questions":[
         4,
         3,
@@ -81,7 +81,7 @@ var friends = [
   },
   {
     "name":"Marine Biologist Barrier Phillips",
-    "photo":"../data/image/ai_face_6.jpg",
+    "photo":"./image/ai_face_6.jpg",
     "questions":[
         3,
         2,
@@ -97,7 +97,7 @@ var friends = [
   },
   {
     "name":"General Westerley Cragg-Smith",
-    "photo":"../data/images/ai_face_7.jpg",
+    "photo":"./images/ai_face_7.jpg",
     "questions":[
         3,
         2,
@@ -113,7 +113,7 @@ var friends = [
   },
   {
     "name":"Bassoonist Bille Lavendor",
-    "photo":"../data/images/ai_face_8.jpg",
+    "photo":"./images/ai_face_8.jpg",
     "questions":[
         5,
         4,
@@ -129,7 +129,7 @@ var friends = [
   },
   {
     "name":"Chef Rockwell D. Jones",
-    "photo":"../data/images/ai_face_9.jpg",
+    "photo":"./images/ai_face_9.jpg",
     "questions":[
         1,
         2,
@@ -161,7 +161,7 @@ var friends = [
   },
   {
     "name":"Mortgage Broker Indigo Marks",
-    "photo":"../data/image/ai_face_11.jpg",
+    "photo":"./image/ai_face_11.jpg",
     "questions":[
         2,
         4,
@@ -177,7 +177,7 @@ var friends = [
   },
   {
     "name":"Odd Duck Quellen Darner",
-    "photo":"../data/images/ai_face_12.jpg",
+    "photo":"./images/ai_face_12.jpg",
     "questions":[
         2,
         4,
